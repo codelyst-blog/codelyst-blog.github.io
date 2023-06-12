9b38629d187a39254231defc522ac6f956b0aa57
sunjungAn <dkstjswjd83@naver.com>
2023-06-12 23:24:00 +0900

    fixed untracked files

--- out/_next/static/chunks/ae51ba48-d279628840c4a5b2.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[260],{60155:function(t,e,n){n.d(e,{lIk:function(){return i}});var r=n(88357);function i(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{fill:"none",strokeLinecap:"round",strokeMiterlimit:"10",strokeWidth:"32",d:"M80 160h352M80 256h352M80 352h352"}}]})(t)}}}]);
