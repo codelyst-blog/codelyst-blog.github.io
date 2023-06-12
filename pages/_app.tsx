fc806ac9747e2e84426372c12db46a42126f230e
sunjungAn <dkstjswjd83@naver.com>
2023-06-12 23:09:54 +0900

    Feat: deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../styles/theme";
 import Fonts from "../styles/Fonts";
-import "./styles.css";
+import "../styles/styles.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
