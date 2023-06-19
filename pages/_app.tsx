2a8a2b01f987d99abe3473b8aeb5bacd7e3bb2b0
sunjungAn <dkstjswjd83@naver.com>
2023-06-20 00:57:37 +0900

    feat: google search console

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,14 @@ import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../styles/theme";
 import Fonts from "../styles/Fonts";
 import "../styles/styles.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
+            <Head>
+                <meta name="google-site-verification" content="YxxIg5MPg5-Cy29EpQEA393CH_ts8QR6zeLEBmUg8wQ" />
+            </Head>
             <ChakraProvider theme={theme}>
                 <Fonts />
                 <Component {...pageProps} />
